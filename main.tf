5f1d11513c9cddb3500ed6e1e83fbfdf71eafe25
shah78600 <shameemshah900@gmail.com>
2021-10-26 13:17:56 +0530

    commit for fuction app

--- main.tf
+++ main.tf
@@ -1,29 +1,35 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
-
-  required_version = ">= 0.11"
-}
-
 provider "azurerm" {
   features {}
 }
-data "azurerm_client_config" "current" {}
- 
-resource "azurerm_resource_group" "tamopsrg" {
-  name     = "tamops-tf"
-  location = "eastus2"
+resource "azurerm_resource_group" "example" {
+  name     = "azure-functions-test-rg"
+  location = "West Europe"
 }
- 
-resource "azurerm_storage_account" "tamopssa" {
-  name                     = "tamopssatf"
-  resource_group_name      = azurerm_resource_group.tamopsrg.name
-  location                 = azurerm_resource_group.tamopsrg.location
+
+resource "azurerm_storage_account" "example" {
+  name                     = "functionsapptestsa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-} 
+}
 
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}

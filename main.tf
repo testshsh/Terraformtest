c75d632435ca1ba384847682f79ef9d3fe5c7ce6
shah78600 <shameemshah900@gmail.com>
2021-10-26 15:17:42 +0530

    testing cont

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "terraformtest" {
-  name     = "azure-functions-test-rg78600"
+  name     = "azure-functions-test-rg786001"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "terraformtest1" {
-  name                     = "satestingteraform786"
+  name                     = "satestingteraform7861"
   resource_group_name      = azurerm_resource_group.terraformtest.name
   location                 = azurerm_resource_group.terraformtest.location
   account_tier             = "Standard"
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "terraformtest1" {
   }
 
 resource "azurerm_app_service_plan" "terraformtest2" {
-  name                = "azure-functions-test-service-plan20"
+  name                = "azure-functions-test-service-plan201"
   location            = azurerm_resource_group.terraformtest.location
   resource_group_name = azurerm_resource_group.terraformtest.name
 
@@ -27,7 +27,7 @@ resource "azurerm_app_service_plan" "terraformtest2" {
 }
 
 resource "azurerm_function_app" "terraformtest3" {
-  name                       = "test-azure-functions"
+  name                       = "test-azure-functions7860"
   location                   = azurerm_resource_group.terraformtest.location
   resource_group_name        = azurerm_resource_group.terraformtest.name
   app_service_plan_id        = azurerm_app_service_plan.terraformtest2.id

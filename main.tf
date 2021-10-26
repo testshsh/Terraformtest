fd4c3f868b64f2d9f21d10b432551d76f67d71df
shah78600 <shameemshah900@gmail.com>
2021-10-26 14:47:42 +0530

    new code 2

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "functionsapptestsa"
+  name                     = "functionsapptestsa1"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"

74abbc66a9a3c173ea1e47e597cfb53fa0e89d78
shah78600 <shameemshah900@gmail.com>
2022-02-23 18:42:04 +0530

    test

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "Test-Poc"
-    storage_account_name  = "testpoc1"
-    container_name        = "testingtfstate"
-    key                   = "terraform.tfstate"
-  }
-}

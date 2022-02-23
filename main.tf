9d88596a3c02f87498d6b3f0347736e72f566053
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-23 16:32:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,31 @@
+terraform {
+
+  required_version = ">=0.14"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "terraformtest" {
-  name     = "azure-functions-test-rg2378600451"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg-demo" {
+  name     = "rg-${var.namespace}-demo"
+  location = var.location
 }
 
-resource "azurerm_storage_account" "terraformtest1" {
-  name                     = "satestingteraform780661"
-  resource_group_name      = azurerm_resource_group.terraformtest.name
-  location                 = azurerm_resource_group.terraformtest.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg-demo.name
+  location                 = azurerm_resource_group.rg-demo.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    environment = var.environment
   }
-
-resource "azurerm_app_service_plan" "terraformtest2" {
-  name                = "azure-functions-test-service-plan201"
-  location            = azurerm_resource_group.terraformtest.location
-  resource_group_name = azurerm_resource_group.terraformtest.name
-
-  sku {
-    tier = "Standard"
-    size = "S2"
-  }
-}
-
-resource "azurerm_function_app" "terraformtest3" {
-  name                       = "test-azure-functions7860"
-  location                   = azurerm_resource_group.terraformtest.location
-  resource_group_name        = azurerm_resource_group.terraformtest.name
-  app_service_plan_id        = azurerm_app_service_plan.terraformtest2.id
-  storage_account_name       = azurerm_storage_account.terraformtest1.name
-  storage_account_access_key = azurerm_storage_account.terraformtest1.primary_access_key
 }

4c1e2c8bf10c6eba86037d9d249a09048afba8da
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-24 12:27:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 terraform {
-
-  required_version = ">=0.14"
-  
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "<storage_account_name>"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
 }
 
 provider "azurerm" {

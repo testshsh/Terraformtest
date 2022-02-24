9b66e218e93c1d4469cc5adebd8cefc99995afb2
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-24 18:40:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "resource_group_name" {
-  value = azurerm_storage_account.dev.resource_group_name
-}
-output "resource_group_name" {
-  value = azurerm_storage_account.qa.resource_group_name
-}
-output "resource_group_name" {
-  value = azurerm_storage_account.prod.resource_group_name
+  value = azurerm_resource_group.resource_group.name
+  description = "Name of the resource group"
 }

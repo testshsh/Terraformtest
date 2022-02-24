268eb392d0900d071075f1545ccf6b2e178917a6
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-24 15:47:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "resource_group_name" {
-  value = azurerm_storage_account.example.resource_group_name
+  value = azurerm_storage_account.dev.resource_group_name
+}
+output "resource_group_name" {
+  value = azurerm_storage_account.qa.resource_group_name
+}
+output "resource_group_name" {
+  value = azurerm_storage_account.prod.resource_group_name
 }

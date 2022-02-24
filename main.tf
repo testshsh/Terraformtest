a753c2d669f9c0bdd45b1c9205bc2e021a3564c2
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-24 18:40:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "rg-demo" {
-  name     = "rg-${var.namespace}-demo"
+resource "azurerm_resource_group" "rg-name" {
+  name  = var.rg-name
   location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "test" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg-demo.name
-  location                 = azurerm_resource_group.rg-demo.location
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.account_replication_type
 

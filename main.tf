21b1462e3a4ddaa0887d437898f2fdef996f1e99
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-24 14:52:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.97.0"
     }
   }
+}
     backend "azurerm" {
         resource_group_name  = "test-poc"
         storage_account_name = "teststate"

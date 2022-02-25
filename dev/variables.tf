a6f8e45b7fa45d3f36a7f68257e256f49ea62f7e
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-25 19:21:53 +0530

    Create variables.tf

---
+++ dev/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+}
+
+
+variable "location" {
+  description = "Location of Azure reource"
+}
+
+variable "storage_account_tier" {
+    description = "Stoarge Account Tier"
+}
+
+variable "account_replication_type" {
+    description = "Stoarge Account Replication Type"
+}
+
+variable "storage_account_name" {
+    description = "Name of the storage acount"
+}
+
+variable "environment" {
+    description = "The environment in which the resources
+}

6af197c3e434c765ccd08a70549f32ea35652e62
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-25 17:17:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,24 @@
-variable "rg-name" {
-  description = "The name of the resource group are deployed such as dev, qa, prod"
+variable "resource_group_name" {
+  description = "Resource group name"
 }
 
 
-variable namespace {
-    description = "The namespace in which the resources are deployed such as dev, qa, prod"
+variable "location" {
+  description = "Location of Azure reource"
 }
 
-variable location {
-    description = "The location in which the resources are deployed such as east US, west Europe"
-}
-
-variable storage_account_tier {
+variable "storage_account_tier" {
     description = "Stoarge Account Tier"
 }
 
-variable account_replication_type {
+variable "account_replication_type" {
     description = "Stoarge Account Replication Type"
 }
 
-variable storage_account_name {
+variable "storage_account_name" {
     description = "Name of the storage acount"
 }
 
-variable environment {
+variable "environment" {
     description = "The environment in which the resources are deployed such as dev, qa, prod"
 }

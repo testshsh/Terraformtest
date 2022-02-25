3f17b08c60cff439dcdd6afb57ee33d5176fb876
shah78600 <93125130+shah78600@users.noreply.github.com>
2022-02-25 17:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "rg" {
-  name  = var.rg-name
+  name  = "${var.resource_group_name}-${var.environment}"
   location = var.location
 }
 
 resource "azurerm_storage_account" "test-gl-poc" {
-  name                     = var.storage_account_name
+  name                     = "${var.storage_account_name}-${var.environment}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = var.storage_account_tier
